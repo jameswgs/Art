a5017eb36dfbce915182aa47968fae8c336bf5bc
jameswgs <james_shephard@hotmail.com>
2018-11-21 22:02:57 +0000

    Extracting a render target...

---
+++ src/main/kotlin/net/slenderloris/art/LinesDebug.kt
@@ -0,0 +1,142 @@
+package net.slenderloris.art
+
+import processing.core.PApplet
+import processing.core.PConstants
+import processing.core.PGraphics
+import processing.core.PVector
+import processing.event.KeyEvent
+import kotlin.math.sin
+
+fun main(args: Array<String>) {
+    PApplet.main("net.slenderloris.art.LinesDebug")
+}
+
+class LinesDebug : PApplet() {
+
+    private var frameNumber = 0
+
+    private val smokey = Smokey(this)
+
+    override fun settings() {
+        fullScreen()
+    }
+
+    override fun setup() {
+
+        val gfx = createGraphics(width, height)
+
+        val seed: Long = 5
+        noiseSeed(seed)
+        randomSeed(seed)
+        background(0)
+
+        smokey.setup()
+    }
+
+    override fun draw() {
+        smokey.draw(frameNumber)
+
+        //background(0)
+        //image(smokey.gfx, 0.0f, 0.0f)
+
+        frameNumber++
+    }
+
+    override fun keyPressed(event: KeyEvent) {
+        super.keyPressed(event)
+        if (event.key == 's') {
+            saveFrame("frame-#######.png")
+        }
+    }
+
+}
+
+class Smokey(private val pApplet: PApplet) {
+
+    var gfx: PApplet = pApplet
+
+    private lateinit var points: List<Particle>
+    private var zoom = 2.0f
+    private val noiseScaleStart = 0.0025f
+    private val noiseScaleIncreaseFactor = 0.0025f
+    private val timeScale = 0.3f
+    private val forceScale = 0.25f
+    private val bias = PVector(0.0f, 0.0f)
+    private val spread = 150.0f
+
+    fun setup() {
+
+        points = emptyList()
+
+        gfx.colorMode(PConstants.HSB, 255.0f)
+
+        while(points.size < 10000) {
+            points += createRndParticle1()
+            points += createRndParticle2()
+        }
+    }
+
+    fun draw(frameNumber: Int) {
+
+        gfx.colorMode(PConstants.HSB, 255.0f)
+
+        // gfx.blendMode(PConstants.ADD)
+
+        val fFrameNo = frameNumber.toFloat()
+
+        val thetaStart = -PConstants.HALF_PI
+        val noiseScaleTheta = noiseScaleIncreaseFactor * fFrameNo + thetaStart
+        val sinScale = 1.0f
+        val noiseScaleFactor = (sinScale + 1.0f) + sin(noiseScaleTheta) * sinScale
+        val noiseScale = noiseScaleStart * noiseScaleFactor
+
+        PApplet.println("noiseScaleTheta = $noiseScaleTheta, noiseScaleFactor = $noiseScaleFactor, noiseScale = $noiseScale")
+
+        points.forEach { point ->
+            gfx.stroke(point.colour)
+            val pos = point.pos
+            val noiseVector = pApplet.noiseVector(pos.x, pos.y, fFrameNo * timeScale, noiseScale)
+            val v2 = noiseVector * forceScale
+            val oldPos = pos.copy()
+            pos.add(v2).add(bias)
+            drawLine(oldPos, pos)
+        }
+
+    }
+
+    private fun createRndParticle1(): Particle {
+        val x = pApplet.random(spread) - spread / 2 - 100.0f
+        val y = pApplet.random(spread) - spread / 2
+        val col = gfx.color(0.0f, 255.0f, 255.0f, 3.0f)
+        return Particle(PVector(x, y), col)
+    }
+
+    private fun createRndParticle2(): Particle {
+        val x = pApplet.random(spread) - spread / 2 + 100.0f
+        val y = pApplet.random(spread) - spread / 2
+        val col = gfx.color(128.0f, 255.0f, 255.0f, 3.0f)
+        return Particle(PVector(x, y), col)
+    }
+
+    private fun drawLine(oldPos: PVector, pos: PVector) {
+        gfx.line(
+                oldPos.x * zoom + gfx.width / 2,
+                oldPos.y * zoom + gfx.height / 2,
+                pos.x * zoom + gfx.width / 2,
+                pos.y * zoom + gfx.height / 2
+        )
+    }
+
+}
+
+
+private fun PApplet.noiseVector(x: Float, y: Float, z: Float, noiseScale: Float): PVector {
+    val noise = noise(x * noiseScale, y * noiseScale, z * noiseScale)
+    val theta = noise * Math.PI.toFloat() * 20.0f
+    return PVector(1.0f, 0.0f).rotate(theta)
+}
+
+
+private operator fun PVector.times(a: Float): PVector {
+    return this.mult(a)
+}
